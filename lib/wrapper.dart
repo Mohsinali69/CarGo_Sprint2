0f60b2190e94cadf236bc3541f4e054ab0ced68b
Mohsin Ali <ali.m@graduate.utm.my>
2023-05-21 15:22:00 +0800

    first commit

---
+++ lib/wrapper.dart
@@ -0,0 +1,23 @@
+import 'package:rent_my_ride_s1/authenticate/authenticate.dart';
+import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
+import 'explore_page.dart';
+import 'model/MyUser.dart';
+
+class Wrapper extends StatelessWidget {
+  const Wrapper({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    final user = Provider.of<MyUser?>(context);
+
+    if (user == null) {
+      Navigator.of(context).pushReplacement;
+      return Authenticate();
+    } else {
+      Navigator.of(context).pushReplacement;
+
+      return ExplorePage();
+    }
+  }
+}
